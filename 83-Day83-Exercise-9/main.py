f399803535501d0591f0db60848f6bc72dbc42c3
Archit Jain <jain.archit0274@gmail.com>
2026-08-01 15:21:10 +0530

    Add name pronunciation program using win32 API
    Implement a program to pronounce names using win32 API.

---
+++ 83-Day83-Exercise-9/main.py
@@ -0,0 +1,11 @@
+Write a program to pronounce list of names using win32 API. 
+If you are given a list l as follows:
+```python
+l = ["Rahul", "Nishant", "Archit"]
+```
+Your program should pronouce:
+```
+Shoutout to Rahul
+Shoutout to Nishant
+Shoutout to Archit
+```
